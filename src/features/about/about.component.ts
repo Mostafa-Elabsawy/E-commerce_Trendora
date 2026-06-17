be1770a22fe3c82ec703b53f95aa509564b62ca7
Mostafa-Elabsawy <mostfaelabsawy@gmail.com>
2026-06-17 21:40:39 +0300

    ready for merge

--- src/features/about/about.component.ts
+++ src/features/about/about.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../../../shared/components/footer/footer/footer.component';
-import { HeaderComponent } from '../../../shared/components/header/header/header.component';
-
 @Component({
   selector: 'app-about',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
--- src/features/contact/contact.component.ts
+++ src/features/contact/contact.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../../../shared/components/footer/footer/footer.component';
-import { HeaderComponent } from '../../../shared/components/header/header/header.component';
-
 @Component({
   selector: 'app-contact',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [ ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
